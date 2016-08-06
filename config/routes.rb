0a4b4680a3c1dc6146c70fe39d9333b6475de2e2
ronil2k <ronil2kgmail.com>
2016-08-06 14:28:44 +1000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
+  resources :users
+  get 'users/new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
