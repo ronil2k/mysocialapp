7caa08d1ea59feb1cbcf1e06f33aa1045dd7d53b
ronil2k <ronil2kgmail.com>
2016-09-21 20:40:41 +1000

    changes

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
@@ -11,5 +11,6 @@ module Mysocialapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.active_record.raise_in_transactional_callbacks = true
   end
 end
